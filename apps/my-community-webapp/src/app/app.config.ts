be0d0c7214dbe2c5df7b82669ca9cc667a900fb9
Tomasz Flis <tomaszwwl@gmail.com>
2024-12-17 21:24:26 +0100

    init proiject

--- apps/my-community-webapp/src/app/app.config.ts
+++ apps/my-community-webapp/src/app/app.config.ts
@@ -1,15 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    NG_EVENT_PLUGINS,
   ],
 };
