e508d646ae8ba931043fa663d03991e27a6433e2
Tomasz Flis <tomaszwwl@gmail.com>
2024-12-18 20:28:55 +0100

    introduce ngrx

--- apps/my-community-webapp/src/app/app.config.ts
+++ apps/my-community-webapp/src/app/app.config.ts
@@ -7,9 +7,13 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideStore(),
+    provideEffects(),
     provideAnimations(),
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
